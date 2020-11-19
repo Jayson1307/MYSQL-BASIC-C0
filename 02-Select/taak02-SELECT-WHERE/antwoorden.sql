62d4969acff115e2a9d99a19fc0a9c590a77720b
jayson van olffen <jayson.vanolffen@student.rocva.nl>
2020-11-19 20:33:36 +0100

    taak 3

--- 02-Select/taak02-SELECT-WHERE/antwoorden.sql
+++ 02-Select/taak02-SELECT-WHERE/antwoorden.sql
@@ -1,7 +1,7 @@
 -- Opdracht 1 
-
+SELECT * FROM jaar2015 WHERE rank = 7
 -- Opdracht 2 
-
+SELECT * FROM `jaar2016` WHERE country = 'Netherlands'
 -- Opdracht 3
 
 -- Opdracht 4
@@ -9,9 +9,9 @@
 -- Opdracht 5
 
 -- Opdracht 6 
-
+SELECT country FROM jaar2016 WHERE rank = 25
 -- Opdracht 7 
-
+SELECT country FROM jaar2016 WHERE score > 7087
 -- Opdracht 8
 
 -- Opdracht 9
