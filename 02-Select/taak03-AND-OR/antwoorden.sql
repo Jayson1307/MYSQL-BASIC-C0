b26c3bd0cf3e2fc5a79c28142a497e2f9e8ae568
jayson van olffen <jayson.vanolffen@student.rocva.nl>
2020-11-22 20:01:22 +0100

    halferwege

--- 02-Select/taak03-AND-OR/antwoorden.sql
+++ 02-Select/taak03-AND-OR/antwoorden.sql
@@ -1,17 +1,17 @@
 -- Opdracht 1 
-
+SELECT * FROM players WHERE nationality = 'spain' AND club = 'chelsea'
 -- Opdracht 2 
-
+SELECT * FROM players WHERE age = 17 AND nationality = 'spain' and wage = 15000
 -- Opdracht 3
 
 -- Opdracht 4
 
 -- Opdracht 5
-
+SELECT * FROM players WHERE club = 'Ajax' AND not nationality = 'netherlands'
 -- Opdracht 6 
-
+SELECT name, age FROM players WHERE club = 'AZ Alkmaar'
 -- Opdracht 7 
-
+SELECT name, age, club FROM players WHERE club = 'AZ Alkmaar'
 -- Opdracht 8
 
 -- Opdracht 9
