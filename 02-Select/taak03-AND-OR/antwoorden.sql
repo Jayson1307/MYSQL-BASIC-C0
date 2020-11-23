0c3d68d01d6b83ff5926b62ae383b4578f0b11ed
jayson van olffen <jayson.vanolffen@student.rocva.nl>
2020-11-23 10:41:41 +0100

    and or af

--- 02-Select/taak03-AND-OR/antwoorden.sql
+++ 02-Select/taak03-AND-OR/antwoorden.sql
@@ -13,17 +13,17 @@ SELECT name, age FROM players WHERE club = 'AZ Alkmaar'
 -- Opdracht 7 
 SELECT name, age, club FROM players WHERE club = 'AZ Alkmaar'
 -- Opdracht 8
-
+SELECT name, wage FROM players WHERE club = 'Manchester City'
 -- Opdracht 9
-
+SELECT name FROM players where age = 30 and wage < 10000
 -- Opdracht 10
-
+SELECT name, age FROM players WHERE nationality = 'spain' OR nationality = 'portugal'
 -- Opdracht 11
-
+SELECT name, age, club FROM players WHERE nationality = 'portugal' OR club = 'chelsea'
 -- Opdracht 12
 
 -- Opdracht 13
-
+SELECT * FROM players WHERE nationality = 'netherlands' and club = 'Ajax' OR club = 'FC Utrecht'
 -- Opdracht 14
 
 -- Opdracht 15
