add72f9c207a5820f15e7a021120b181df9ee800
jayson van olffen <jayson.vanolffen@student.rocva.nl>
2020-11-23 11:49:06 +0100

    delete opdracht 1 klaar

--- 02-Select/taak04-Functions-Alias/antwoorden.sql
+++ 02-Select/taak04-Functions-Alias/antwoorden.sql
@@ -1,9 +1,9 @@
 -- Opdracht 1 
-
+SELECT MAX(wage) AS hoogste_loon_speler_fc_utrecht FROM players
 -- Opdracht 2 
-
+SELECT ROUND(AVG(wage)) AS avg_wage_iedereen FROM players
 -- Opdracht 3
-
+select SUM(wage) AS SUM_wage FROM players WHERE club = 'FC Groningen'
 -- Opdracht 4
 
 -- Opdracht 5
@@ -13,7 +13,7 @@
 -- Opdracht 7 
 
 -- Opdracht 8
-
+SELECT COUNT(*) AS spelers_Chelasea FROM players WHERE club = 'Chelsea'
 -- Opdracht 9
 
 -- Opdracht 10
